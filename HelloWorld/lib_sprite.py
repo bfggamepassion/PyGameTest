dede8c601f7cbb29a31ec0374c8c6689f96012b4
LOUVET Michel <michel.louvet@businessdecision.com>
2023-05-24 17:11:09 +0200

    Ajout sprite

--- HelloWorld/lib_sprite.py
+++ HelloWorld/lib_sprite.py
@@ -13,9 +13,21 @@ class Lutin:
         self.nb_frames = 0                      # Nombre d'images composants l'animation
         self.frame_delay = 0                    # Temps de pause entre les images composants l'animation
         self.anim_loop = True                   # L'animation boucle t'elle ?
-        self.action_end_loop = 0                # Action en fin d'animation :
-                                                #    0 - Boucle ou pas selon anim_loop
-                                                #    1 - Désactive le Lutin
-        def affiche_lutin(screen):              # 
+        self.action_end_loop = 0                # Action en fin d'animation : 0 - Boucle ou pas selon anim_loop / 1 - Désactive le Lutin
+        self.current_frame=0
+        self.inner_counter=0
+        
+        def affiche_lutin(screen,image_bank):   
+            screen.blit(image_bank[self.frames[self.current_frame]], (self.pos_x, self.pos_y)) 
 
-        def maj_lutins():                       # Met a jour les animations de lutin
+        def maj_lutins():                                    # Met a jour les animations de lutin
+            if (self.inner_counter==self.frame_delay):       # delay atteint pour image suivante
+                self.inner_counter = 0                       # remise du compteur a 0
+                if (self.current_frame==self.nb_frames):     # on est à la dernière frame ?
+                    if (self.anim_loop==True):               # doit on boucler ?
+                        self.current_frame=0                 # si oui on boucle
+                else:                                        # on est pas à la dernière frame
+                    self.current_frame++                     # on passe alors à la suivante
+            else:                                            # delay non atteind
+                self.inner_counter++                         # on incrémente le compteur
+                    
